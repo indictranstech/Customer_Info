beb37f912ea6b19ee7a30458014f2f24c525a2d8
khatrijitendra <jitendra.k@indictranstech.com>
2016-03-08 16:47:29 +0530

    [Change module OF item Category]

---
+++ customer_info/customer_info/doctype/item_category/item_category.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2015, indictrans and contributors
+# For license information, please see license.txt
+
+from __future__ import unicode_literals
+import frappe
+from frappe.model.document import Document
+
+class ItemCategory(Document):
+	pass
+
+@frappe.whitelist()
+def new_item_group(category_name):
+	group = frappe.new_doc("Item Group")
+	group.item_group_name = category_name
+	group.parent_item_group = "All Item Groups"
+	group.is_group = "No"
+	group.save(ignore_permissions = True)
+	return "True"
