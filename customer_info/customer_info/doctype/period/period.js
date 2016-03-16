caec3b28d0b835933f0501e42e967691b1e1b6b7
khatrijitendra <jitendra.k@indictranstech.com>
2016-03-16 18:44:01 +0530

    [Feature Develop]

--- customer_info/customer_info/doctype/period/period.js
+++ customer_info/customer_info/doctype/period/period.js
@@ -1,4 +1,4 @@
 frappe.ui.form.on("Period","period",function(frm){
-	cur_frm.doc.period_name = cur_frm.doc.period + " " +"mėn"
+	cur_frm.doc.period_name = cur_frm.doc.period + " " +"mėn."
 	refresh_field("period_name")
 })
