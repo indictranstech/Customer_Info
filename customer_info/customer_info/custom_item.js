ad1ea62b2283b9c3891fbf9ead72b0a84ad396b0
khatrijitendra <jitendra.k@indictranstech.com>
2016-03-21 19:06:00 +0530

    [remove duplicacy from code]

--- customer_info/customer_info/custom_item.js
+++ customer_info/customer_info/custom_item.js
@@ -1,56 +1,36 @@
-/*cur_frm.add_fetch('product_category', 'period_value', 'period');
-cur_frm.add_fetch('product_category', 'ratio_value', 'ratio');*/
-
-/*frappe.ui.form.on("Item","product_category",function(frm){
-	cur_frm.add_fetch('product_category', 'period_value', 'period');
-	cur_frm.add_fetch('product_category', 'ratio_value', 'ratio');
-	refresh_field("period")	
-})*/
-
-/*frappe.ui.form.on("Item","purchase_date",function(frm){
-     var d = moment().format('YYYY-MM-DD')
-     var current_date = new Date(d)
-     var expected_start_date = new Date(cur_frm.doc.purchase_date)
-     if(expected_start_date < current_date){
-          msgprint(__(" 'Purchase Date' Should Not Past Date "))
-          cur_frm.doc.purchase_date = ""
-          refresh_field('purchase_date')
-     }         
-})*/
-
-frappe.ui.form.on("Item", "product_category", function(frm){
+frappe.ui.form.on("Item", {
+    product_category: function(frm){
     if(cur_frm.doc.product_category){
-          frappe.call({
-                  method: "frappe.client.get_value",
-                  args: {
-                        doctype: "Item Category",
-                        fieldname: ["period_value","ratio_value","period"],
-                        filters: { name: cur_frm.doc.product_category },
-                  },
+            frappe.call({
+                method: "frappe.client.get_value",
+                args: {
+                    doctype: "Item Category",
+                    fieldname: ["period_value","ratio_value","period"],
+                    filters: { name: cur_frm.doc.product_category },
+                },
              	callback: function(res){
-                	      if (res && res.message){
-                		      cur_frm.doc.period = res.message['period_value']
-                              cur_frm.doc.ratio = res.message['ratio_value']
-                		      cur_frm.doc.agreement_period = res.message['period']
-                              refresh_field(["period","ratio","agreement_period"]);
-                 	      }
+                    if(res && res.message){
+                        cur_frm.doc.period = res.message['period_value']
+                        cur_frm.doc.ratio = res.message['ratio_value']
+                        cur_frm.doc.agreement_period = res.message['period']
+                        refresh_field(["period","ratio","agreement_period"]);
+                    }
              	}  	
-          });
-      }    
+            });
+            cur_frm.doc.item_group = cur_frm.doc.product_category
+            refresh_field("item_group")        
+        }
+    },
+    serial_number: function(frm){
+        cur_frm.doc.item_code = cur_frm.doc.brand + "-" +cur_frm.doc.serial_number
+        cur_frm.doc.item_name = cur_frm.doc.item_code
+        refresh_field(["item_code","item_name"])    
+    },
+    purchase_price_with_vat: function(frm){
+        this.calculation_for_monthly_rental_and_90d(frm)
+    }
+
 });
-
-
-frappe.ui.form.on("Item","serial_number",function(frm){
-	cur_frm.doc.item_code = cur_frm.doc.brand + "-" +cur_frm.doc.serial_number
-	cur_frm.doc.item_name = cur_frm.doc.item_code
-	refresh_field(["item_code","item_name"])	
-})
-
-
-frappe.ui.form.on("Item","product_category",function(frm){
-	cur_frm.doc.item_group = cur_frm.doc.product_category
-	refresh_field("item_group")
-})
 
 cur_frm.fields_dict['brand'].get_query = function(doc) {
 	return {
@@ -60,7 +40,7 @@ cur_frm.fields_dict['brand'].get_query = function(doc) {
 	}
 }
 
-frappe.ui.form.on("Item","purchase_price_with_vat",function(frm){
+calculation_for_monthly_rental_and_90d = function(frm){    
 	var b = (cur_frm.doc.purchase_price_with_vat * cur_frm.doc.ratio) / cur_frm.doc.period
 	var c = roundNumber(b,2)
 	var n = c.toString();
@@ -68,68 +48,67 @@ frappe.ui.form.on("Item","purchase_price_with_vat",function(frm){
 	var aa = parseInt(n1[0])
 	if(aa >= 5){
 		var five = Math.floor(b) + 0.99
-          if(five >= 0){
-		   cur_frm.doc.monthly_rental_payment = five
-		   refresh_field("monthly_rental_payment")
-	     }
-          else{
-             cur_frm.doc.monthly_rental_payment = "0"
-             refresh_field("monthly_rental_payment")
-          }
-     }
+        if(five >= 0){
+            cur_frm.doc.monthly_rental_payment = five
+            refresh_field("monthly_rental_payment")
+	    }
+        else{
+            cur_frm.doc.monthly_rental_payment = "0"
+            refresh_field("monthly_rental_payment")
+        }
+    }
 	else{
 		var less = Math.floor(b) - 0.01
 		if(less >= 0){
-               cur_frm.doc.monthly_rental_payment = less
-		     refresh_field("monthly_rental_payment")
-	     }
-          else{
-             cur_frm.doc.monthly_rental_payment = "0"
-             refresh_field("monthly_rental_payment")
-          }
-     }
-     if(cur_frm.doc.purchase_price_with_vat >= 1 && cur_frm.doc.purchase_price_with_vat <= 430.99){
-          var div = (cur_frm.doc.purchase_price_with_vat * 15) / 100
-          var de = cur_frm.doc.purchase_price_with_vat + div
-          calculation_for_90d_sac(de)
-     }            
-     else if(cur_frm.doc.purchase_price_with_vat >= 431 && cur_frm.doc.purchase_price_with_vat <= 580.99){
-          var div = (cur_frm.doc.purchase_price_with_vat * 12) / 100
-          var de = cur_frm.doc.purchase_price_with_vat + div
-          calculation_for_90d_sac(de)                  
-     }
-     else if(cur_frm.doc.purchase_price_with_vat >= 581){
-          var div = (cur_frm.doc.purchase_price_with_vat * 10) / 100
-          var de = cur_frm.doc.purchase_price_with_vat + div
-          calculation_for_90d_sac(de)      
-     }
-})            
+            cur_frm.doc.monthly_rental_payment = less
+            refresh_field("monthly_rental_payment")
+	    }
+        else{
+            cur_frm.doc.monthly_rental_payment = "0"
+            refresh_field("monthly_rental_payment")
+        }
+    }
+    if(cur_frm.doc.purchase_price_with_vat >= 1 && cur_frm.doc.purchase_price_with_vat <= 430.99){
+        var div = (cur_frm.doc.purchase_price_with_vat * 15) / 100
+        var de = cur_frm.doc.purchase_price_with_vat + div
+        calculation_for_90d_sac(de)
+    }            
+    else if(cur_frm.doc.purchase_price_with_vat >= 431 && cur_frm.doc.purchase_price_with_vat <= 580.99){
+        var div = (cur_frm.doc.purchase_price_with_vat * 12) / 100
+        var de = cur_frm.doc.purchase_price_with_vat + div
+        calculation_for_90d_sac(de)                  
+    }
+    else if(cur_frm.doc.purchase_price_with_vat >= 581){
+        var div = (cur_frm.doc.purchase_price_with_vat * 10) / 100
+        var de = cur_frm.doc.purchase_price_with_vat + div
+        calculation_for_90d_sac(de)      
+    }
+}            
 
 calculation_for_90d_sac = function(de){
-     
-     var deduc = de.toFixed(2)
-     var str_deduc = deduc.toString()
-     var slice_str_deduc = str_deduc.slice(-2)
-     var int_slice_str_deduc = parseInt(slice_str_deduc)
-     
-     var floor = Math.floor(deduc)
-     var str_floor = floor.toString();
-     var slice_str_floor = str_floor.slice(-1)
-     var int_str_floor = parseInt(slice_str_floor)
+    var deduc = de.toFixed(2)
+    var str_deduc = deduc.toString()
+    var slice_str_deduc = str_deduc.slice(-2)
+    var int_slice_str_deduc = parseInt(slice_str_deduc)
 
-     var concade = slice_str_floor + "." + slice_str_deduc
-     var float_concade = parseFloat(concade)
+    var floor = Math.floor(deduc)
+    var str_floor = floor.toString();
+    var slice_str_floor = str_floor.slice(-1)
+    var int_str_floor = parseInt(slice_str_floor)
+
+    var concade = slice_str_floor + "." + slice_str_deduc
+    var float_concade = parseFloat(concade)
 
 
-     if(float_concade >= 0.00 && float_concade <= 5.00){
-          var minus = 5 - int_str_floor
-          cur_frm.set_value("90d_sac_price",(floor + minus))
-          refresh_field("90d_sac_price")
-     }
+    if(float_concade >= 0.00 && float_concade <= 5.00){
+      var minus = 5 - int_str_floor
+      cur_frm.set_value("90d_sac_price",(floor + minus))
+      refresh_field("90d_sac_price")
+    }
 
-     if(float_concade >= 5.01 && float_concade <= 9.99){
-          var minus = 9 - int_str_floor
-          cur_frm.set_value("90d_sac_price",(floor + minus))
-          refresh_field("90d_sac_price")
-     }      
+    if(float_concade >= 5.01 && float_concade <= 9.99){
+      var minus = 9 - int_str_floor
+      cur_frm.set_value("90d_sac_price",(floor + minus))
+      refresh_field("90d_sac_price")
+    }      
 }
--- customer_info/customer_info/custom_script_customer.js
+++ customer_info/customer_info/custom_script_customer.js
@@ -1,101 +1,103 @@
-frappe.ui.form.on("Customer","onload",function(frm){
-    if(cur_frm.doc.customer_type == "Individual"){
-        cur_frm.set_df_property("first_name", "reqd",true);
-    }
-})
-
-frappe.ui.form.on("Customer","customer_type",function(frm){
-    if(cur_frm.doc.customer_type == "Company"){
-       cur_frm.set_df_property("first_name", "reqd",false);
-       cur_frm.set_df_property("company_title", "reqd",true);
-       cur_frm.doc.customer_group = "Commercial"
-       refresh_field('customer_group')      
-    }
-    if(cur_frm.doc.customer_type == "Individual"){
-       cur_frm.set_df_property("first_name", "reqd",true);
-       cur_frm.set_df_property("company_title", "reqd",false);
-       cur_frm.doc.customer_group = "Individual"
-       refresh_field('customer_group')      
-    }
-})
-
-frappe.ui.form.on("Customer","first_name",function(frm){
-    if(cur_frm.doc.customer_type == "Individual"){
-        cur_frm.doc.customer_name = cur_frm.doc.first_name
-        refresh_field("customer_name")
-    } 
-    if(cur_frm.doc.first_name){
-        var n = cur_frm.doc.first_name;    
-        if(n.slice(-1) == "s" || n.slice(-1) == "S"){
-            cur_frm.doc.gender = "M"
-            refresh_field("gender")
+frappe.ui.form.on("Customer",{
+    onload:function(frm){
+        if(cur_frm.doc.customer_type == "Individual"){
+            cur_frm.set_df_property("first_name", "reqd",true);
+        }
+        this.get_url(frm)
+    },
+    customer_type:function(frm){
+        if(cur_frm.doc.customer_type == "Company"){
+           cur_frm.set_df_property("first_name", "reqd",false);
+           cur_frm.set_df_property("company_title", "reqd",true);
+           cur_frm.doc.customer_group = "Commercial"
+           refresh_field('customer_group')      
+        }
+        if(cur_frm.doc.customer_type == "Individual"){
+           cur_frm.set_df_property("first_name", "reqd",true);
+           cur_frm.set_df_property("company_title", "reqd",false);
+           cur_frm.doc.customer_group = "Individual"
+           refresh_field('customer_group')      
+        }    
+    },
+    company_title: function(frm){
+        if(cur_frm.doc.customer_type == "Company" && cur_frm.doc.company_title){
+            cur_frm.doc.customer_name = cur_frm.doc.company_title
+            refresh_field("customer_name")
+        }
+    },
+    first_name: function(frm){
+        if(cur_frm.doc.customer_type == "Individual"){
+            cur_frm.doc.customer_name = cur_frm.doc.first_name
+            refresh_field("customer_name")
+            var n = cur_frm.doc.first_name;    
+            if(n.slice(-1) == "s" || n.slice(-1) == "S"){
+                cur_frm.doc.gender = "M"
+                refresh_field("gender")
+            }
+            else{
+                cur_frm.doc.gender = "F"
+                refresh_field("gender")
+            }
         }
         else{
-            cur_frm.doc.gender = "F"
-            refresh_field("gender")
+            cur_frm.doc.gender = ""
+            refresh_field('gender')
+        }    
+    },
+    last_name: function(frm){
+        if(cur_frm.doc.customer_type == "Individual" && cur_frm.doc.first_name){
+            cur_frm.doc.customer_name = cur_frm.doc.first_name +" "+ cur_frm.doc.last_name
+            refresh_field("customer_name")
         }
-    }
-    else{
-        cur_frm.doc.gender = ""
-        refresh_field('gender')
-    }
-})
-
-frappe.ui.form.on("Customer","last_name",function(frm){
-    if(cur_frm.doc.customer_type == "Individual"){
-        cur_frm.doc.customer_name = cur_frm.doc.first_name +" "+ cur_frm.doc.last_name
-        refresh_field("customer_name")
-    }
-})
-
-frappe.ui.form.on("Customer","company_title",function(frm){
-    if(cur_frm.doc.customer_type == "Company"){
-        cur_frm.doc.customer_name = cur_frm.doc.company_title
-        refresh_field("customer_name")
-    }
-})
-
-frappe.ui.form.on("Customer","birthdate",function(frm){
-    if(cur_frm.doc.birthdate){
-        var d = moment().format('YYYY-MM-DD')
-        var current_date = new Date(d)
-        var b_date = new Date(cur_frm.doc.birthdate)
-        if(b_date >= current_date){
-            msgprint(__("'Birth Date' Not Greater Or Equal To Current Date"))
-            cur_frm.doc.birthdate = ""
-            refresh_field('birthdate')
+    },
+    birthdate: function(frm){
+        if(cur_frm.doc.birthdate){
+            var d = moment().format('YYYY-MM-DD')
+            var current_date = new Date(d)
+            var b_date = new Date(cur_frm.doc.birthdate)
+            if(b_date >= current_date){
+                msgprint(__("'Birth Date' Not Greater Or Equal To Current Date"))
+                cur_frm.doc.birthdate = ""
+                refresh_field('birthdate')
+            }
+        } 
+        if(cur_frm.doc.birthdate){
+            var birthdate = new Date(cur_frm.doc.birthdate)
+            var current_date = new Date();
+            var diff = current_date - birthdate; // This is the difference in milliseconds
+            var age = Math.floor(diff/31536000000); // Divide by 1000*60*60*24*365
+            cur_frm.doc.age = age + " " +" Years"
+            refresh_field('age');
         }
-    } 
-    if(cur_frm.doc.birthdate){
-        var birthdate = new Date(cur_frm.doc.birthdate)
-        var current_date = new Date();
-        var diff = current_date - birthdate; // This is the difference in milliseconds
-        var age = Math.floor(diff/31536000000); // Divide by 1000*60*60*24*365
-        cur_frm.doc.age = age + " " +" Years"
-        refresh_field('age');
+        else{
+            cur_frm.doc.age = ""
+            refresh_field('age')
+        }    
+    },
+    customer_is_interested_in: function(frm){
+        this.get_url(frm)
+    },
+    prersonal_code: function(frm){
+        var tempVal = cur_frm.doc.prersonal_code;
+        if(!/^\d+$/.test(tempVal)){
+            msgprint(__("Enter Digits From [0-9] Only"))
+            cur_frm.doc.prersonal_code = "",
+            refresh_field("prersonal_code")  
+        }
+        else if (!/^[0-9]{1,11}$/.test(tempVal)){ // OR if (/^[0-9]{1,10}$/.test(+tempVal) && tempVal.length<=10) 
+            msgprint(__("Personal Code Length Not Greater Than 11 Digits"))
+            cur_frm.doc.prersonal_code = "",
+            refresh_field("prersonal_code")
+        }
+        /*else if (cur_frm.doc.prersonal_code.length < 11){
+            msgprint(__("Personal Code Should Be Of 11 Digits"))
+            cur_frm.doc.prersonal_code = "",
+            refresh_field("prersonal_code")
+        }*/
     }
-    else{
-        cur_frm.doc.age = ""
-        refresh_field('age')
-    } 
 })
 
-// frappe.ui.form.on("Customer","customer_is_interested_in",function(frm){
-//     if (cur_frm.doc.customer_is_interested_in) {
-//         $(frm.fields_dict['click_here'].wrapper)
-//             .html(repl('<div class="row">\
-//                 <label class="control-label" style="margin-left: 16px;">Visit URL</label></div>\
-//                 <div class="row">\
-//                 <a target="_blank" href=%(link)s style="margin-left:26px">%(link)s%0A</a>%0A\
-//                 </div>', {
-//                     link: cur_frm.doc.customer_is_interested_in
-//                 }))
-//         refresh_field("click_here")                                        
-//     }
-//  /*   cur_frm.doc.set_df_property("customer_is_interested_in","hidden",1)*/
-// })
-
-frappe.ui.form.on("Customer", "customer_is_interested_in", function(frm,doctype,name) {
+get_url = function(frm){
     if(cur_frm.doc.customer_is_interested_in){
         var html='';
         html+=repl('<div class="row">\
@@ -109,40 +111,5 @@ frappe.ui.form.on("Customer", "customer_is_interested_in", function(frm,doctype,
                     })
 
         $(cur_frm.fields_dict.click_here.wrapper).html(html);
-    }    
-});
-
-frappe.ui.form.on("Customer", "onload", function(frm,doctype,name) {
-    if(cur_frm.doc.customer_is_interested_in){
-        var html='';
-        html+=repl('<div class="row">\
-                    <label class="control-label" style="margin-left: 16px;">Visit URL</label></div>\
-                    <div class="row">\
-                    <a target="_blank" href=%(link)s style="margin-left:26px">%(show_link)s</a>\
-                    </div>', {
-                        link: cur_frm.doc.customer_is_interested_in,
-                        show_link: cur_frm.doc.customer_is_interested_in.slice(0,40)
-                    })
-
-        $(cur_frm.fields_dict.click_here.wrapper).html(html);
-    } 
-});
-
-frappe.ui.form.on("Customer", "prersonal_code", function(frm) {
-    var tempVal = cur_frm.doc.prersonal_code;
-    if(!/^\d+$/.test(tempVal)){
-        msgprint(__("Enter Digits From [0-9] Only"))
-        cur_frm.doc.prersonal_code = "",
-        refresh_field("prersonal_code")  
     }
-    else if (!/^[0-9]{1,11}$/.test(tempVal)){ // OR if (/^[0-9]{1,10}$/.test(+tempVal) && tempVal.length<=10) 
-        msgprint(__("Personal Code Length Not Greater Than 11 Digits"))
-        cur_frm.doc.prersonal_code = "",
-        refresh_field("prersonal_code")
-    }
-    /*else if (cur_frm.doc.prersonal_code.length < 11){
-        msgprint(__("Personal Code Should Be Of 11 Digits"))
-        cur_frm.doc.prersonal_code = "",
-        refresh_field("prersonal_code")
-    }*/
-})    
+} 
