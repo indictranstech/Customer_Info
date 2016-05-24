3152a63b41faf524c54b5d15d2992d13719df996
khatrijitendra <jitendra.k@indictranstech.com>
2016-05-24 19:36:00 +0530

    [REmove Update from agreement_status]

--- customer_info/customer_info/doctype/customer_agreement/customer_agreement.js
+++ customer_info/customer_info/doctype/customer_agreement/customer_agreement.js
@@ -56,16 +56,35 @@ frappe.ui.form.on("Customer Agreement",{
                 refresh_field('s90d_sac_price')
             }
         }
-        if(cur_frm.doc.document_type == "Updated"){
+        /*if(cur_frm.doc.document_type == "Updated"){
             cur_frm.set_df_property("agreement_period","read_only",0)
             cur_frm.set_df_property("agreement_status","read_only",1)
             refresh_field("agreement_period","agreement_status")
-        }    
+        }*/
+        if(cur_frm.doc.document_type == "New" && cur_frm.doc.__islocal){
+            cur_frm.set_df_property("agreement_status","hidden",1)
+        }
+        if(cur_frm.doc.document_type == "Updated" && cur_frm.doc.__islocal){
+            cur_frm.set_df_property("without_update_agreement_status","hidden",1)
+            /*cur_frm.set_df_property("agreement_status","hidden",0)*/
+        }
+        if(cur_frm.doc.document_type == "Updated" && !cur_frm.doc.__islocal){
+            cur_frm.set_df_property("without_update_agreement_status","hidden",1)
+            cur_frm.set_df_property("agreement_status","hidden",0)
+        }
+        if(cur_frm.doc.document_type == "New" && cur_frm.doc.agreement_status == "Updated"){
+            cur_frm.set_df_property("without_update_agreement_status","hidden",1)
+            cur_frm.set_df_property("agreement_status","hidden",0)   
+        }
     },
     validate:function(frm){
         if(cur_frm.doc.__islocal){
             cur_frm.doc.date = frappe.datetime.nowdate()
             refresh_field("date")
+        }
+        if(cur_frm.doc.document_type == "New" && cur_frm.doc.__islocal){    
+            cur_frm.doc.agreement_status = cur_frm.doc.without_update_agreement_status
+            refresh_field("agreement_status")
         }
         if(cur_frm.doc.agreement_status && cur_frm.doc.__islocal){
             cur_frm.doc.old_agreement_status = cur_frm.doc.agreement_status
@@ -113,34 +132,15 @@ frappe.ui.form.on("Customer Agreement",{
             refresh_field("agreement_no")
         }
     },
-    /*address: function(frm){
-        if(cur_frm.doc.customer && cur_frm.doc.address){
-            frappe.call({
-                method: "customer_info.customer_info.doctype.customer_agreement.customer_agreement.get_address",
-                args: {
-                    "customer": cur_frm.doc.customer,
-                    "address": cur_frm.doc.address
-                },
-                callback: function(r){
-                    if(r.message){     
-                        cur_frm.doc.address_line1 = r.message[0]["address_line1"]
-                        cur_frm.doc.city = r.message[0]["city"]
-                        refresh_field(["address","address_line1","city"]);
-                    }
-                    if(r.message[0]['address_line2']){
-                        cur_frm.doc.full_address = r.message[0]["address_line1"] + "\n" + r.message[0]["address_line2"] + "\n" + r.message[0]["city"]
-                        cur_frm.doc.address_line2 = r.message[0]["address_line2"]
-                        refresh_field(['full_address','address_line2'])
-                    }
-                    if(!r.message[0]['address_line2']){
-                        cur_frm.doc.address_line2 = ""
-                        cur_frm.doc.full_address = r.message[0]["address_line1"] + "\n" + r.message[0]["city"]
-                        refresh_field(["full_address","address_line2"])
-                    }
-                }   
-            }); 
+    agreement_status:function(frm){
+        console.log("in agreement_status 123")
+        if(cur_frm.doc.agreement_status == "Closed" || cur_frm.doc.agreement_status == "Updated" && !cur_frm.doc.__islocal){
+            console.log("in agreement_status")
+            cur_frm.doc.agreement_status_changed_date = frappe.datetime.nowdate()
+            refresh_field("agreement_status_changed_date")
         }
-    },*/    
+    }
+
 })
 
 cur_frm.fields_dict['product'].get_query = function(doc) {
@@ -155,11 +155,3 @@ make_update_agreement  = function() {
         frm: cur_frm
     })
 }
-
-/*cur_frm.fields_dict['address'].get_query=function(doc){
-	return {
-		filters:{
-			'customer':doc.customer
-		}
-	}
-}*/
