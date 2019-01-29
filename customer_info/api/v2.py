a5f2dcefec97fd996fe1b39c78cf087fc45b4289
jsukrut <surkut.j@indictranstech.com>
2019-01-29 13:52:37 +0530

    [Be 0286 & Be 0289]

--- customer_info/api/v2.py
+++ customer_info/api/v2.py
@@ -8,7 +8,7 @@ from frappe.utils import nowdate, getdate,add_months,add_days,get_last_day
 from frappe.utils import date_diff,flt
 from customer_info.customer_info.doctype.customer_agreement.customer_agreement import payments_done_by_api
 from customer_info.customer_info.doctype.payments_management.make_payment_history import make_payment_history
-
+from customer_info.customer_info.doctype.customer_agreement.customer_agreement import closed_agreement_notification
 
 
 """
@@ -258,7 +258,7 @@ def set_values_in_agreement(customer_agreement):
 				customer_agreement.agreement_closing_suspending_reason = "Contract Term is over"
 				customer_agreement.merchandise_status = "Agreement over"
 				customer_agreement.agreement_close_date = datetime.now().date()
-				# closed_agreement_notification(customer_agreement.customer,customer_agreement.name)
+				closed_agreement_notification(customer_agreement.customer,customer_agreement.name)
 			customer_agreement.balance = (len(customer_agreement.payments_record) - len(payment_made)) * customer_agreement.monthly_rental_payment
 		
 		customer_agreement.save(ignore_permissions=True)
